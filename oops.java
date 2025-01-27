05a223d0edeb36154d007cfc75bdcf591ce95748
tring-bharath <bharath.a@tringapps.com>
2025-01-27 17:07:46 +0530

    first commit

---
+++ oops.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Student
+{
+    int studentId;
+    int studentName;
+    class Admin
+    {
+        int fees;
+        int balance;
+        int paid;
+    }
+    class  Class
+    {
+        int attendance;
+        int average;
+    }
+    class sports
+    {
+        boolean cricket=false;
+        boolean football=false;
+        boolean athletics=false;
+    }
+}
+class oops
+{
+    public static void main(String []args)
+    {
+
+    }
+}
